88d4f98bc6cf6fc752059fcbd3469b131853d233
Spirit <daodeczynn@mail.ru>
2025-04-08 13:55:47 +0200

    Первый коммит для заливки проекта на GitHub

---
+++ portal/src/Components/Blocks/Header.jsx
@@ -0,0 +1,33 @@
+import "../Styles/Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <a href="#">
+        <div className="logo-container">
+          <div className="svg-container">
+            <div className="logo-svg"></div>
+          </div>
+          <h2 className="logo-header">Portal</h2>
+        </div>
+      </a>
+
+      <ul className="header-menu">
+        <li className="menu-item">
+          <a href="#">Игры</a>
+        </li>
+        <li className="menu-item">
+          <a href="#">Студии</a>
+        </li>
+        <li className="menu-item">
+          <a href="#">Подборки</a>
+        </li>
+        <li className="menu-item">
+          <a href="#">Новости</a>
+        </li>
+      </ul>
+
+      <button>Войти</button>
+    </header>
+  );
+}
